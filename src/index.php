3d0edfc47d8f8bfe068644618ce46252784e0f7c
Markus Wößner <markus.woessner@adition.com>
2023-01-16 11:56:23 +0100

    Setup testing environment

--- src/index.php
+++ src/index.php
@@ -3,8 +3,6 @@ require "vendor/autoload.php";
 
 use Adition\XMLProcessor;
 
-
-
 $xmlProcessor = new XMLProcessor();
 $result = $xmlProcessor->process(__DIR__ . "/nums.xml");
 
---
+++ tests/XMLProcessorTest.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace Adition\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class XMLProcessorTest extends TestCase
+{
+    public function testProcess(): void
+    {
+    }
+}
